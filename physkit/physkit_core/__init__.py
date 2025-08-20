299fef008d6e73b23194c020020978fdce3293cb
Yinghuan Zhang <sherryyh.z@gmail.com>
2025-08-20 16:53:55 -0400

    Centralized PhysKitLogger

---
+++ physkit/physkit_core/__init__.py
@@ -0,0 +1,11 @@
+"""
+PhysKit Core Package
+
+This package provides core functionality for the PhysKit framework.
+"""
+
+from .logging_config import PhysKitLogger
+
+__all__ = [
+    "PhysKitLogger"
+]
